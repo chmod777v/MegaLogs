25ddccac19b1d0a78824b5945ea6737bac01ef00
chmod777v <ivan.pylaev1@gmail.com>
2025-09-20 20:53:48 +0300

    CreateConfig UpdateMain

--- main.go
+++ main.go
@@ -4,6 +4,24 @@ import (
 	"encoding/json"
 	"log/slog"
 	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	httpRequests = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "Total HTTP requests",
+	}, []string{"method", "status"})
+
+	responseTime = promauto.NewHistogram(prometheus.HistogramOpts{
+		Name:    "http_response_time_seconds",
+		Help:    "Response time in seconds",
+		Buckets: []float64{0.1, 0.5, 1, 2},
+	})
 )
 
 type Base struct {
@@ -12,26 +30,39 @@ type Base struct {
 }
 
 func Get(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
 	req := Base{
 		Number: 123,
 		String: "Hellow, world",
 	}
-
 	bytes, err := json.MarshalIndent(req, "", " ")
 	if err != nil {
 		slog.Error("Error json.Marshal")
+		httpRequests.WithLabelValues("GET", "500").Inc()
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
 	w.Write(bytes)
+	httpRequests.WithLabelValues("GET", "200").Inc()
+	responseTime.Observe(time.Since(start).Seconds())
 	slog.Info("Sucessfull post Request")
 }
 func Post(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
 	var req Base
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		httpRequests.WithLabelValues("POST", "500").Inc()
 		w.WriteHeader(http.StatusInternalServerError)
 		slog.Error("Erro while receiving data", "ERROR", err.Error())
 		return
 	}
+
+	httpRequests.WithLabelValues("POST", "200").Inc()
+	responseTime.Observe(time.Since(start).Seconds())
 	slog.Info("RequestGet", "Data", req)
 }
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -40,10 +71,14 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		Get(w, r)
 	case http.MethodPost:
 		Post(w, r)
+	default:
+		httpRequests.WithLabelValues(r.Method, "405").Inc()
+		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
 
 }
 func main() {
+	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/", Handler)
 
 	slog.Info("Server listening", "Host", "localhost:8080")
